f215b14cccd578720abea4b2f8945a28a09e595f
Mike Mercer <mmercer@trivir.com>
2024-09-23 13:03:39 -0600

    try

--- src/ops/PromoteOps.ts
+++ src/ops/PromoteOps.ts
@@ -1,8 +1,12 @@
 import { frodo, FrodoError, state } from '@rockcarver/frodo-lib'
 import { FullExportOptions } from '@rockcarver/frodo-lib/types/ops/ConfigOps';
+import { printError } from '../utils/Console';
+const { saveJsonToFile, getFilePath } = frodo.utils;
+const { exportFullConfiguration } = frodo.config;
 
 export async function compareExportToDirectory(
   exp: string,
+  includeMeta = true,
   dir: string,
   options: FullExportOptions = {
     useStringArrays: true,
@@ -13,5 +17,17 @@ export async function compareExportToDirectory(
     target: '',
   }
 ): Promise<boolean> {
-  return true;
+  try {
+    const collectErrors: Error[] = [];
+    const exportData = await exportFullConfiguration(options, collectErrors);
+    let fileName = 'all.config.json';
+    saveJsonToFile(exportData, getFilePath(fileName, true), includeMeta);
+    if (collectErrors.length > 0) {
+      throw new FrodoError(`Errors occurred during full export`, collectErrors);
+    }
+    return true;
+  } catch (error) {
+    printError(error);
+  }
+  return false;
 }

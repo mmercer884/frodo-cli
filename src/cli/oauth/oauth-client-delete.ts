5d49c4ae6def6f8f23b05536adb8df84a0c2b392
Michael Mercer <mmercer@trivir.com>
2025-02-24 12:23:55 -0700

    fix changes after rebase

--- src/cli/oauth/oauth-client-delete.ts
+++ src/cli/oauth/oauth-client-delete.ts
@@ -3,8 +3,6 @@ import { Option } from 'commander';
 import { getTokens } from '../../ops/AuthenticateOps';
 import { deleteOauth2ClientById } from '../../ops/OAuth2ClientOps';
 import { FrodoCommand } from '../FrodoCommand';
-import { deleteOauth2ClientById } from '../../ops/OAuth2ClientOps';
-import { FrodoCommand } from '../FrodoCommand';
 
 export default function setup() {
   const program = new FrodoCommand('frodo oauth client delete');
--- src/ops/PromoteOps.ts
+++ src/ops/PromoteOps.ts
@@ -183,53 +183,6 @@ export async function compareExportToDirectory(
   return false;
 }
 
-function enviornmentChanged(files: CompareObj): boolean {
-  // variables
-  let variable = files.changed.find((val) => val.includes('global/variable'));
-  if (variable) {
-    return true;
-  }
-  variable = files.added.find((val) => val.includes('global/variable'));
-  if (variable) {
-    return true;
-  }
-  variable = files.deleted.find((val) => val.includes('global/variable'));
-  if (variable) {
-    return true;
-  }
-  // secrets
-  // variable = files.changed.find((val) => val.includes('global/secret'));
-  // if (variable) {
-  //   return true;
-  // }
-  // variable = files.added.find((val) => val.includes('global/secret'));
-  // if (variable) {
-  //   return true;
-  // }
-  // variable = files.deleted.find((val) => val.includes('global/secret'));
-  // if (variable) {
-  //   return true;
-  // }
-}
-
-export async function effectDifferences(
-  compObj: CompareObj,
-  masterDir: string,
-  exportDir: string,
-  effectSecrets: boolean = false
-) {
-  for (const add of compObj.added) {
-    await addFile(add, masterDir, effectSecrets);
-  }
-  for (const change of compObj.changed) {
-    await changeFile(change, masterDir, effectSecrets);
-  }
-  for (const del of compObj.deleted) {
-    await deleteFile(del, exportDir, effectSecrets);
-  }
-  verboseMessage(`finished effect differences`);
-}
-
 /**
  * checks to see if there were any changes to esv's to see if an environment refresh is necessary
  * @param files the compare object we need to filter through to see if any variables or secrets were changed
